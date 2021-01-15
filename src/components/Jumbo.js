ea39acb4eafbaafa65decc274e7ddc70fd606821
devferx <ferkki@hotmail.com>
2021-01-15 15:47:32 -0400

    Clase Usando React para manejar la parte visual e interactiva de nuestra aplicación

---
+++ src/components/Jumbo.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { StyledJumbo } from "../styles/components"
+
+export default function Jumbo() {
+  return (
+    <StyledJumbo>
+      <div>
+        <h2>¡Consigue el mejor swag exclusivo y espcial de Platzi!</h2>
+        <small>Piezasa elegantes para los mejores estudiantes</small>
+      </div>
+    </StyledJumbo>
+  )
+}
