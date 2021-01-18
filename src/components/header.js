7bd1b6cc7060669b4c0b1aea488d93d36bfbbc2f
devferx <ferkki@hotmail.com>
2021-01-18 10:06:39 -0400

    Clase Construyendo el Carrito de Compras: useContext

--- src/components/header.js
+++ src/components/header.js
@@ -1,38 +1,43 @@
+import React, { useContext } from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import { CartContext } from "../context"
 import { MenuItem, StyledHeader } from "../styles/components"
 
-const Header = () => (
-  <StyledHeader>
-    <Link to="/">
-      <img
-        src="https://i.postimg.cc/6q3pg48v/Logo.png"
-        alt="Logo Platzi Swag"
-      />
-    </Link>
-    <nav>
-      <ul>
-        <MenuItem margin>
-          <Link to="/">Productos</Link>
-        </MenuItem>
-        <MenuItem margin>
-          <a href="http://www.platzi.cpm">Platzi</a>
-        </MenuItem>
-        <MenuItem>
-          <Link to="/cart">
-            <span>
-              <img
-                src="https://i.postimg.cc/L6wpMxLt/cart.png"
-                alt="cart icon"
-              />
-            </span>
-          </Link>
-        </MenuItem>
-      </ul>
-    </nav>
-  </StyledHeader>
-)
+const Header = () => {
+  const { cart } = useContext(CartContext)
+  return (
+    <StyledHeader>
+      <Link to="/">
+        <img
+          src="https://i.postimg.cc/6q3pg48v/Logo.png"
+          alt="Logo Platzi Swag"
+        />
+      </Link>
+      <nav>
+        <ul>
+          <MenuItem margin>
+            <Link to="/">Productos</Link>
+          </MenuItem>
+          <MenuItem margin>
+            <a href="http://www.platzi.cpm">Platzi</a>
+          </MenuItem>
+          <MenuItem>
+            <Link to="/cart">
+              <span>
+                <img
+                  src="https://i.postimg.cc/L6wpMxLt/cart.png"
+                  alt="cart icon"
+                />
+                {cart.length}
+              </span>
+            </Link>
+          </MenuItem>
+        </ul>
+      </nav>
+    </StyledHeader>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
